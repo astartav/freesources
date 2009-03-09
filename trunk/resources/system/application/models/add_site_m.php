9b8390025eb1995adbdeaa8c7df47d8281ab6ac0
Astarta.V <Astarta.V@c22c2bc2-0a89-11de-b993-7fcebbf76996>
2009-03-09 17:41:20 +0000

    После того как ты мне кинул три файла

--- trunk/resources/system/application/models/add_site_m.php
+++ trunk/resources/system/application/models/add_site_m.php
@@ -1,36 +1,39 @@
 <?php
-class Add_site_m extends Model{
- var $parentid='';
- var $name='';
- var $link='';
- var $parts='';
- var $programs='';
- var $categories='';
- var $blocks='';
- var $ourrating='';
- var $language='';
- var $smalldesc='';
- var $fulldesc='';
+class Add_site_m extends Model {
+  var $parentid='';
+  var $name='';
+  var $link='';
+  var $parts='';
+  var $programs='';
+  var $categories='';
+  var $blocks='';
+  var $ourrating='';
+  var $language='';
+  var $smalldesc='';
+  var $fulldesc=''; 
  function Add_site_m(){
  
- parent::Model;
+ parent::Model();
  
  }
- function insert_into_db(){
-  $this->parentid->$POST['parentid'];
-  $this->name->$POST['sitename'];
-  $this->link->$POST['link'];
-  $this->parts->$POST['parts'];
-  $this->programs->$POST['programs'];
-  $this->categories->$POST['categories'];
-  $this->blocks->$POST['blocks'];
-  $this->ourrating->$POST['ourrating'];
-  $this->language->$POST['language'];
-  $this->smalldesc->$POST['smalldesc'];
-  $this->fulldesc->$POST['fulldesc'];
+ 
+ function insert_into_db() {
   
-  $this-db->insert('sites', $this);
+  $this->parentid->$_POST['parentid'];
+  $this->name->$_POST['sitename'];
+  $this->link->$_POST['link'];
+  $this->parts->$_POST['parts'];
+  $this->programs->$_POST['programs'];
+  $this->categories->$_POST['categories'];
+  $this->blocks->$_POST['blocks'];
+  $this->ourrating->$_POST['ourrating'];
+  $this->language->$_POST['language'];
+  $this->smalldesc->$_POST['smalldesc'];
+  $this->fulldesc->$_POST['fulldesc'];
+  
+  $this->db->insert('sites', $this);
  }
  
 }
 ?>
+
