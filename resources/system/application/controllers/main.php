333eebb8ec76998f153d8ad9ebc151944ee2337c
Astarta.V <Astarta.V@c22c2bc2-0a89-11de-b993-7fcebbf76996>
2009-03-10 16:42:38 +0000

    Рабочие скрипты загрузки и вывода (кодировка только бьется)

--- resources/system/application/controllers/main.php
+++ resources/system/application/controllers/main.php
@@ -1,10 +1,25 @@
 <?php
 class Main extends Controller {
 
-	function index()
-	{    
-	 $this->load->helper('form');	
-	 $this->load->view('main_v');
-	}
+ function index() {
+
+  $this->load->view('main_v');
+  $this->load->model('Main_m');
+  $all_sites = $this->Main_m->index_m(); 
+  foreach ($all_sites->result() as $row) {
+  
+   echo $row->parentid;
+   echo $row->name;
+   echo $row->link;
+   echo $row->smalldesc;
+   echo $row->fulldesc;
+   echo $row->language;
+   echo $row->ourrating;
+ 
+  }
+ 
+ }
+
+
 }
 ?>
--- resources/system/application/models/add_site_m.php
+++ resources/system/application/models/add_site_m.php
@@ -19,17 +19,17 @@ class Add_site_m extends Model {
  
  function insert_into_db() {
   
-  $this->parentid->$_POST['parentid'];
-  $this->name->$_POST['sitename'];
-  $this->link->$_POST['link'];
-  $this->parts->$_POST['parts'];
-  $this->programs->$_POST['programs'];
-  $this->categories->$_POST['categories'];
-  $this->blocks->$_POST['blocks'];
-  $this->ourrating->$_POST['ourrating'];
-  $this->language->$_POST['language'];
-  $this->smalldesc->$_POST['smalldesc'];
-  $this->fulldesc->$_POST['fulldesc'];
+  $this->parentid = $_POST['parentid'];
+  $this->name = $_POST['sitename'];
+  $this->link = $_POST['link'];
+  $this->parts = $_POST['parts'];
+  //$this->programs = $_POST['programs'];
+  //$this->categories = $_POST['categories'];
+  //$this->blocks = $_POST['blocks'];
+  $this->ourrating = $_POST['ourrating'];
+  $this->language = $_POST['language'];
+  $this->smalldesc = $_POST['smalldesc'];
+  $this->fulldesc = $_POST['fulldesc'];
   
   $this->db->insert('sites', $this);
  }
