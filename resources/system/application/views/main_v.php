6a889c14ff1c7c74619022e18d538ad8a0390977
Astarta.V <Astarta.V@c22c2bc2-0a89-11de-b993-7fcebbf76996>
2009-03-14 17:17:51 +0000

    Исправила. См. красный спэн

--- resources/system/application/views/main_v.php
+++ resources/system/application/views/main_v.php
@@ -4,31 +4,42 @@
 <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
 <title>Main Page</title>
 <style>
-.body_right {
+.head {
+position: absolute;
+width: 100%;
+height: 10%;
+top: 0%;
+left: 0%;
+background: red;
+}
+.body_sites {
  position: absolute;
+ top: 15%;
  left: 35%;
  width: 30%;
- background: orange;
  padding: 5px;
 }
 </style>
 </head>
 <body>
+<div class="head">
+<h1>The Best Links Ever</h1>
 <a style="background: silver;" href="http://localhost/index.php/add_site">Add site</a>
-<div class="body_right">
+</div>
+<div class="body_sites">
 <?php 
 foreach ($all_sites->result() as $row) {
  if ($row->ourrating>90){?>
-  <a href="<?php echo $row->link;?>"><?php echo $row->name;?></a><br>
-  <a style="font-size: 9px;" href="category<?php echo $row->parentid;?>">Категория <?php echo $row->parentid;?></a>
+  <div style="background: silver;"><a href="<?php echo $row->link;?>"><?php echo $row->name;?></a><span style="position: absolute; right: 5px; background: red;">99</span><br>
+  <a style="font-size: 9px;" href="category<?php echo $row->parentid;?>">Категория <?php echo $row->parentid;?></a></div>
    <?php
-   echo "<br>";  
+   //echo "<br>";  
    echo $row->smalldesc; 
    echo "<br>";
    //echo $row->fulldesc;
    //echo $row->language;
    //echo $row->ourrating;?>
-   <div style="text-align: right;"><a href="id">info</a></div>
+   <div style="text-align: right; background: yellow;"><a href="id">info</a> | <a href="idcomment">комментировать (23)</a></div>
    <hr>
     <?php }} ?>
      </div>
