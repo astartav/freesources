bbe3b6b7753505ebd8901656ea86d693a15c3583
Astarta.V <Astarta.V@c22c2bc2-0a89-11de-b993-7fcebbf76996>
2009-03-15 14:20:55 +0000

    Выводит на глагне два блока, только в верстке фигня.

--- resources/system/application/models/main_m.php
+++ resources/system/application/models/main_m.php
@@ -1,7 +1,32 @@
 <?php
 class Main_m extends Model {
- function index_m(){
- $sites = $this->db->query('SELECT * FROM sites');
+ function programming(){
+ $sites = $this->db->query('SELECT * FROM sites WHERE parts="Программирование"');
+ return $sites;
+
+ }
+function webdesign(){
+ $sites = $this->db->query('SELECT * FROM sites WHERE parts="Веб-дизайн"');
+ return $sites;
+
+ }
+function design(){
+ $sites = $this->db->query('SELECT * FROM sites WHERE parts="Дизайн"');
+ return $sites;
+
+ }
+function art(){
+ $sites = $this->db->query('SELECT * FROM sites WHERE parts="Арт"');
+ return $sites;
+
+ }
+function flash(){
+ $sites = $this->db->query('SELECT * FROM sites WHERE parts="Флеш"');
+ return $sites;
+
+ }
+function photography(){
+ $sites = $this->db->query('SELECT * FROM sites WHERE parts="Фотография"');
  return $sites;
 
  }
